d92b98f0198a438298c8a05bc63e94fea70855d5
Hendy Irawan <hendy@soluvas.com>
2012-05-02 23:09:00 +0700

    new puppet module project

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'ceefour-magento'
+version ''
+source ''
+author 'ceefour'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: magento
+#
+# This module manages magento
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class magento {
+
+
+}
